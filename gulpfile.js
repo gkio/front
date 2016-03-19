be3f1e211a737e610760ab9c761d9512b06de0b7
gkio <webdevra@bk.ru>
2016-03-19 23:16:03 +0200

    almost ready project

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-var gulp    = require('gulp')
-var postcss = require('gulp-postcss');
-var watch   = require('gulp-watch')
-var cssnext = require('postcss-cssnext')
-var swig = require('gulp-swig')
+var gulp       = require('gulp')
+var postcss    = require('gulp-postcss');
+var watch      = require('gulp-watch')
+var cssnext    = require('postcss-cssnext')
+var swig       = require('gulp-swig')
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', function() {
+  return gulp.src('compiled/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('compiled/min-css/'));
+});
 
 
 gulp.task('swig', function() {
   gulp.src('./magicGoesHere/swig/*.html')
     .pipe(swig())
+    .pipe(swig({defaults: { cache: false }}))
     .pipe(gulp.dest('./compiled/html/'))
 });
 
@@ -33,13 +41,13 @@ gulp.task('css', function () {
         				require('postcss-input-style'),
         				 ]))        
         .pipe( gulp.dest('compiled/css/') )
-   
+        
 });
 
 gulp.task('watch',function(){
-	
+    gulp.watch('./compiled/css/*.css',['minify-css'])
     gulp.watch('magicGoesHere/postcss/*.css',['css'])
-	gulp.watch('magicGoesHere/swig/*.swig',['swig'])
+	gulp.watch('magicGoesHere/swig/*.html',['swig'])
 })
 
 gulp.task('default',['css','swig','watch'])
